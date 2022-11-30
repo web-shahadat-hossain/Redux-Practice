4f5bf4459a99e59cd836310ea945dbf5544fdea8
developer-sahadat <mdridoysikder862@gmail.com>
2022-12-01 00:14:53 +0600

    context api react js

--- src/Component/Home/Child.js
+++ src/Component/Home/Child.js
@@ -3,7 +3,15 @@ import { CountContextApi } from "../../App";
 
 const Child = () => {
   const { count, setCount } = useContext(CountContextApi);
-  return <div className="box"></div>;
+  return (
+    <div className="box">
+      <h2>{count}</h2>
+      <div>
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <button onClick={() => setCount((dec) => dec - 1)}>-</button>
+      </div>
+    </div>
+  );
 };
 
 export default Child;
